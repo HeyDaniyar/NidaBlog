f8ff6a734df86f1c75095bdbc34662eccd9065da
Daniyar Saifuding <daniyar1815@gmail.com>
2024-10-18 00:16:14 -0400

    📈 Add clarity analysis

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './blog/[slug]/blog.css'
 
 import { Analytics } from '@vercel/analytics/react'
@@ -14,6 +15,19 @@ export default function BlogLayout({
 }) {
   return (
     <>
+      <Script
+        id="clarity-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_KEY}");
+          `,
+        }}
+      />
       <div className="pointer-events-none fixed inset-0 select-none bg-[url('/grid-black.svg')] bg-top bg-repeat dark:bg-[url('/grid.svg')]" />
       <span className="pointer-events-none fixed top-0 block h-[800px] w-full select-none bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(5,5,5,0.045)_0%,rgba(0,0,0,0)_100%)] dark:bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(255,255,255,0.09)_0%,rgba(255,255,255,0)_100%)]" />
 
